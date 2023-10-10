e0246df1efbdaa1f5d077d5bc09661c0392ea45d
leijingzhan <754369646@qq.com>
2023-10-10 19:36:44 +0800

    first-commit

--- app/src/main/java/com/example/compose_project/Login/LoginScreen.kt
+++ app/src/main/java/com/example/compose_project/Login/LoginScreen.kt
@@ -1,31 +1,5 @@
 package com.example.compose_project.Login
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.compose_project.model.Man
-
-import com.example.ui.TopBar.CenterTopAppBar
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 
 //@OptIn(ExperimentalComposeUiApi::class)
